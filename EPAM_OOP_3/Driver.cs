cae8aeed2497bedb4d1cbeb926aab7cf92e4774f
sirwior <34189374+sirwior@users.noreply.github.com>
2019-09-02 21:08:01 +0300

    Add files via upload

---
+++ EPAM_OOP_3/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_OOP_3
+{
+    public class Driver
+    {
+        public string Name { get; }
+        public int Speed { get; set; }
+
+        public Driver(string name, int speed)
+        {
+            Name = name;
+            Speed = speed;
+        }
+    }
+}
---
+++ EPAM_OOP_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver driver = new Driver("Oleg", 70);
+            Inspector inspector = new Inspector("Sanya");
+            inspector.Offence += Penalty;
+            inspector.CheckSpeed(driver);
+
+            Console.ReadLine();
+
+            
+        }
+
+        private static void Penalty(string nameViolator, int speed)
+        {
+            if ((speed - 60) <= 10 && (speed - 60) > 0)
+            {
+                Console.WriteLine($"{nameViolator} оштрафован на 100 грн.");
+            }
+            if ((speed - 60) <= 20 && (speed - 60) > 10)
+            {
+                Console.WriteLine($"{nameViolator} оштрафован на 200 грн.");
+            }
+            if (speed - 60 > 20)
+            {
+                Console.WriteLine($"{nameViolator} оштрафован на 500 грн.");          
+            }
+        }
+    }
+}
